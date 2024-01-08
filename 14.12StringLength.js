eed5dc5e88f21608bd10b6b78b6a453433f55602
Syed_Hamid <syedhamid1491@gmail.com>
2024-01-08 17:41:20 +0530

    string len

---
+++ 14.12StringLength.js
@@ -0,0 +1,4 @@
+var tweet = prompt("Enter your tweet") ;
+var tweetCount = tweet.length ;
+
+alert("You have written " + tweetCount + " charecter you have " + (180 - tweetCount) + " charecters left") ;
